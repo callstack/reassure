9f98eed91cb4a70bd225ae5eb19a152e870cc53d
TMaszko <tomasz.krzyzowski@callstack.com>
2022-03-03 15:50:43 +0100

    fix: simplified eslint setup
    Signed-off-by: TMaszko <tomasz.krzyzowski@callstack.com>

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,26 +1,9 @@
-const path = require('path');
-const pak = require('../package.json');
-
 module.exports = {
   root: true,
   extends: '@callstack/eslint-config',
-  settings: {
-    'import/core-modules': [
-      'rn-perf-tool',
-      'escape-string-regexp',
-      'metro-config/src/defaults/exclusionList',
-    ],
-    'import/resolver': {
-      node: {
-        moduleDirectory: ['node_modules', 'src/'],
-      },
-      alias: {
-        map: [
-          ['rn-perf-tool', path.join(__dirname, '..', pak.source)],
-          ['components', './src/components'],
-        ],
-        extensions: ['.ts', '.js', '.jsx', '.tsx', '.json'],
-      },
-    },
+  rules: {
+    // typescript will handle that
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
   },
 };
