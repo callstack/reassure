9125b99139316f999862d94325c1ce7585792cd8
xiltyn <jakub.bujko@callstack.com>
2022-03-09 15:17:48 +0100

    test: duplicated test names

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -26,5 +26,5 @@ node --jitless --expose-gc --no-concurrent-sweeping --max-old-space-size=4096 no
 mv "$current_file".txt "$base_file".txt;
 git checkout "$current_branch";
 mv "$current_file"_temp.txt "$current_file.txt";
-node "$rootDir/lib/commonjs/analyser.js" --output=all --baselineFilePath="$base_file.txt" --currentFilePath="$current_file.txt" && node "$rootDir/lib/commonjs/markdown-builder.js";
+node "$rootDir/lib/commonjs/analyser.js" --output=all --baselineFilePath="$base_file.txt" --currentFilePath="$current_file.txt" --unhandled-rejections=strict && node "$rootDir/lib/commonjs/markdown-builder.js" --unhandled-rejections=strict;
 
