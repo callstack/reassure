962ec75e0a97e32b82c3922dc38ca3b62dba8665
TMaszko <tomasz.krzyzowski@callstack.com>
2022-03-09 14:07:03 +0100

    fix: run perf test only against  .perf.test or .perf.spec files
    Signed-off-by: TMaszko <tomasz.krzyzowski@callstack.com>

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -15,13 +15,14 @@ base_branch=${base_branch:="main"}
 base_file=${base_file:="baseline"}
 current_file=${current_file:="current"}
 current_branch=$(git branch --show-current)
+test_files_regex=".*\.perf\.(test|spec)\.(js|ts)x?$"
 
  # ../node_modules/rn-perf-tool/scripts/run-test.sh -> ../node_modules/rn-perf-tool
 rootDir="$(dirname $(dirname $0))"
-node --jitless --expose-gc --no-concurrent-sweeping --max-old-space-size=4096 node_modules/jest/bin/jest.js;
+node --jitless --expose-gc --no-concurrent-sweeping --max-old-space-size=4096 node_modules/jest/bin/jest.js "$test_files_regex";
 mv "$current_file.txt" "$current_file"_temp.txt;
 git checkout "$base_branch";
-node --jitless --expose-gc --no-concurrent-sweeping --max-old-space-size=4096 node_modules/jest/bin/jest.js;
+node --jitless --expose-gc --no-concurrent-sweeping --max-old-space-size=4096 node_modules/jest/bin/jest.js "$test_files_regex";
 mv "$current_file".txt "$base_file".txt;
 git checkout "$current_branch";
 mv "$current_file"_temp.txt "$current_file.txt";
