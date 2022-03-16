3381e1951234803fc71a73692782bbb6c9b2a360
TMaszko <tomasz.krzyzowski@callstack.com>
2022-03-16 17:21:19 +0100

    fix: remove import from danger file and unnecessary markdown builder import from index
    Signed-off-by: TMaszko <tomasz.krzyzowski@callstack.com>

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import dangerJs from './plugins';
-import type { DangerJsConfig } from './src/plugins';
 
 dangerJs({
   inputFilePath: path.join(__dirname, './example/analyser-output.md'),
-} as DangerJsConfig);
+});
--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,3 @@ export {
   configure,
   resetToDefault,
 } from './measure';
-
-import './markdown-builder';
