fb8c702fc11afe2a8a2df48e656d9c289925735a
TMaszko <tomasz.krzyzowski@callstack.com>
2022-03-16 16:41:04 +0100

    fix: typo inside the dangerfile (danger plugin import)
    Signed-off-by: TMaszko <tomasz.krzyzowski@callstack.com>

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import dangerJs from './plugins';
-import type { DangerJsConfig } from './src/plugins/';
+import type { DangerJsConfig } from './src/plugins';
 
 dangerJs({
   inputFilePath: path.join(__dirname, './example/analyser-output.md'),
