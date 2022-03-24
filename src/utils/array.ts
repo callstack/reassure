dc6532176425b032b768cbaff0a2cca9a35c91bc
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-03-24 16:14:12 +0100

    code review changes

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,6 +1,6 @@
 /**
  * Check if array has duplicates.
  */
-export function hasDuplicateValues(elements: string[]) {
+export function hasDuplicateValues<T>(elements: T[]) {
   return elements.length !== new Set(elements).size;
 }
