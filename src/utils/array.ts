c2eff18aaacaf2733414620d826242016bd8b37a
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-03-31 16:00:00 +0200

    self code review

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -4,13 +4,3 @@
 export function hasDuplicateValues<T>(elements: T[]) {
   return elements.length !== new Set(elements).size;
 }
-
-// export function findDuplicates<T>(elements: T[]) {
-//   const duplicates: T[] = [];
-
-//   const counts = new Map<any, number>();
-//   elements.forEach((element) => {
-//     const previousCount = counts.get(element) ?? 0;
-//     counts.set(element);
-//   });
-// }
