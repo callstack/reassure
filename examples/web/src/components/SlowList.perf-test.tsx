c568d526e10b1ac5adb25a1a638a9552bdc5af66
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-06-15 13:47:36 +0200

    experiment: React web support

---
+++ examples/web/src/components/SlowList.perf-test.tsx
@@ -0,0 +1,45 @@
+import { meassurePerformanceWeb as measurePerformance } from '@reassure/reassure';
+
+import React from 'react';
+
+import { fireEvent, RenderResult } from '@testing-library/react';
+
+import { SlowList } from './SlowList';
+
+const AsyncComponent = () => {
+  const [count, setCount] = React.useState(0);
+
+  const triggerStateChange = () => {
+    setTimeout(() => setCount((c) => c + 1), 50);
+  };
+
+  return (
+    <div>
+      <button onClick={triggerStateChange}>Action</button>
+      <p>Count: {count}</p>
+
+      <SlowList count={200} />
+    </div>
+  );
+};
+
+jest.setTimeout(60_000);
+test('Async Component', async () => {
+  const scenario = async (screen: RenderResult) => {
+    const button = screen.getByText('Action');
+
+    fireEvent.click(button);
+    await screen.findByText('Count: 1');
+
+    fireEvent.click(button);
+    await screen.findByText('Count: 2');
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    await screen.findByText('Count: 5');
+  };
+
+  await measurePerformance(<AsyncComponent />, { scenario });
+  expect(true).toBeTruthy();
+});
