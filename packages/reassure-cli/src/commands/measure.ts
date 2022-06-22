f9b0d813ed73f6143041fdd5e9299c9f88ca9c27
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-06-22 06:59:25 +0200

    feature: working measure command

--- packages/reassure-cli/src/commands/measure.ts
+++ packages/reassure-cli/src/commands/measure.ts
@@ -1,6 +1,27 @@
+import * as fs from 'fs';
+import { spawnSync } from 'child_process';
+
+const RESULTS_DIRECTORY = '.reassure';
+const RESULTS_FILE = '.reassure/current.perf';
+
 export const command = 'measure';
 export const describe = 'runs performance tests to gather measurements';
 
-export function handler(argv: any) {
-  console.log('Running measure command', argv);
+export function handler() {
+  fs.mkdirSync(RESULTS_DIRECTORY, { recursive: true });
+  fs.rmSync(RESULTS_FILE, { force: true });
+
+  spawnSync(
+    'node',
+    [
+      '--jitless',
+      '--expose-gc',
+      '--no-concurrent-sweeping',
+      '--max-old-space-size=4096',
+      'node_modules/.bin/jest',
+      '--runInBand',
+      '--testMatch "<rootDir>/**/*.perf-test.[jt]s?(x)"',
+    ],
+    { shell: true, stdio: 'inherit' }
+  );
 }
