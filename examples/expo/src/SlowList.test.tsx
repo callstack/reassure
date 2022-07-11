31683990246d477531ad157a53001901e2fedee0
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-11 22:02:10 +0200

    feature: expo example

---
+++ examples/expo/src/SlowList.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react-native";
+import { SlowList } from "./SlowList";
+
+test("SlowList", () => {
+  render(<SlowList count={10} />);
+
+  const items = screen.getAllByText(/Item/i);
+  expect(items).toHaveLength(10);
+});
