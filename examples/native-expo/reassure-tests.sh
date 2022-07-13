1dfbb9457131339b8744bd3c59ac7cb20b434a28
Michał Pierzchała <thymikee@gmail.com>
2022-07-13 18:51:49 +0200

    add yarn install step after checkout

--- examples/native-expo/reassure-tests.sh
+++ examples/native-expo/reassure-tests.sh
@@ -4,9 +4,11 @@ CURRENT_BRANCH=$(git rev-parse --short HEAD)
 BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Gather baseline perf measurements
-git checkout "$BASELINE_BRANCH";
+git checkout "$BASELINE_BRANCH"
+yarn install
 yarn reassure measure --baseline
 
 # Gather current perf measurements & compare results
-git checkout "$CURRENT_BRANCH";
+git checkout "$CURRENT_BRANCH"
+yarn install
 yarn reassure measure
