ab2617cc63de0b4831b3cc6b12d27e8c98b59830
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-14 18:29:36 +0200

    feature: RTL autodiscovery

--- packages/reassure-measure/src/config.ts
+++ packages/reassure-measure/src/config.ts
@@ -1,8 +1,24 @@
 type Render = (component: React.ReactElement<any>) => any;
 type Cleanup = () => void;
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const RNTL = require('@testing-library/react-native');
+let defaultRender;
+let defaultCleanup;
+
+try {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  const RNTL = require('@testing-library/react-native');
+  defaultRender = RNTL.render;
+  defaultCleanup = RNTL.cleanup;
+} catch (error) {
+  try {
+    // eslint-disable-next-line import/no-extraneous-dependencies
+    const RTL = require('@testing-library/react');
+    defaultRender = RTL.render;
+    defaultCleanup = RTL.cleanup;
+  } catch (error) {
+    console.warn("Reassure: couldn't find neither @testing-library/react-native nor @testing-library/react");
+  }
+}
 
 type Config = {
   runs: number;
@@ -18,8 +34,8 @@ const defaultConfig: Config = {
   dropWorst: 1,
   outputFile: process.env.OUTPUT_FILE ?? '.reassure/current.perf',
   verbose: false,
-  render: RNTL?.render,
-  cleanup: RNTL?.cleanup,
+  render: defaultRender,
+  cleanup: defaultCleanup,
 };
 
 export let config = defaultConfig;
