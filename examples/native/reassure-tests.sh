d09718af524f76824fba2767b95dd9db490ceab7
Michał Pierzchała <thymikee@gmail.com>
2022-07-14 13:38:20 +0200

    Reduce 'reassure meassure' usage to 'reassure' (#87)
    Co-authored-by: Maciej Jastrzebski <mdjastrzebski@gmail.com>

--- examples/native/reassure-tests.sh
+++ examples/native/reassure-tests.sh
@@ -5,8 +5,8 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Gather baseline perf measurements
 git checkout "$BASELINE_BRANCH";
-yarn reassure measure --baseline
+yarn reassure --baseline
 
 # Gather current perf measurements & compare results
 git checkout "$CURRENT_BRANCH";
-yarn reassure measure
+yarn reassure
