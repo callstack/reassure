59afc8257a39ffb65932507499b4851cde27d8f3
Michał Pierzchała <thymikee@gmail.com>
2022-07-15 15:16:48 +0200

    add script

--- examples/web-cra/reassure-tests.sh
+++ examples/web-cra/reassure-tests.sh
@@ -5,12 +5,9 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 TEST_RUNNER_PATH='node_modules/react-scripts/bin/react-scripts.js test'
 
 # Gather baseline perf measurements
-git checkout "$BASELINE_BRANCH";
-CI=true TEST_RUNNER_PATH=$TEST_RUNNER_PATH npx reassure measure --baseline
+git switch "$BASELINE_BRANCH"
+CI=true TEST_RUNNER_PATH=$TEST_RUNNER_PATH npx reassure --baseline
 
-# Gather current perf measurements
-git checkout "$CURRENT_BRANCH";
-CI=true TEST_RUNNER_PATH=$TEST_RUNNER_PATH npx reassure measure
-
-# Compare results
-CI=true TEST_RUNNER_PATH=$TEST_RUNNER_PATH npx reassure compare
+# Gather current perf measurements & compare results
+git switch -
+CI=true TEST_RUNNER_PATH=$TEST_RUNNER_PATH npx reassure
