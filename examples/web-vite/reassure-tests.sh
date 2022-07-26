2cff148c8ff2d31a93af948bc5852376e722276a
Michał Pierzchała <thymikee@gmail.com>
2022-07-26 15:18:52 +0200

    run yarn when switching branches

--- examples/web-vite/reassure-tests.sh
+++ examples/web-vite/reassure-tests.sh
@@ -3,8 +3,10 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
+yarn
 yarn reassure --baseline
 
 # Gather current perf measurements & compare results
 git switch -
+yarn
 yarn reassure
