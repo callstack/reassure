f0e44634c23becdd2fc946f60a37c421b3dc3560
Michał Pierzchała <thymikee@gmail.com>
2022-07-26 14:47:32 +0200

    use reassure@0.4.1

--- examples/web-vite/jest-setup.js
+++ examples/web-vite/jest-setup.js
@@ -1,5 +1,3 @@
 import { configure } from 'reassure';
-import { render, cleanup } from '@testing-library/react';
 
-// Explicitly pass RTL's render and cleanup functions.
-configure({ render, cleanup });
+configure({ testingLibrary: 'react' });
