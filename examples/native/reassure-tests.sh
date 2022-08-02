581d6439449630258c351a356fe8d93efddde521
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-08-02 23:52:32 +0200

    chore: rebuild packages in reassure-tests.sh

--- examples/native/reassure-tests.sh
+++ examples/native/reassure-tests.sh
@@ -4,10 +4,18 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
+
+# Next line is required because Reassure packages are imported from this monorepo and might require rebuilding.
+pushd ../.. && yarn install --force && yarn turbo run build && popd
+
 yarn install --force
 yarn reassure --baseline
 
 # Gather current perf measurements & compare results
 git switch -
+
+# Next line is required because Reassure packages are imported from this monorepo and might require rebuilding.
+pushd ../.. && yarn install --force && yarn turbo run build && popd
+
 yarn install --force
 yarn reassure
