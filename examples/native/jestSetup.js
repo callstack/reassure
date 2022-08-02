75e1584f66779ffde84593ab746a92258c21df9a
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-08-02 15:49:51 +0200

    chore: update react-native in example/native

--- examples/native/jestSetup.js
+++ examples/native/jestSetup.js
@@ -1,3 +1,6 @@
 import { configure } from 'reassure';
 
 configure({ testingLibrary: 'react-native', verbose: true });
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
