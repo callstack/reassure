15ca8fe6840cffec6c5ebff05a536b54d66f3c73
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-08-02 23:33:38 +0200

    feat: add yarn install to reassure-tests.sh

--- examples/native/reassure-tests.sh
+++ examples/native/reassure-tests.sh
@@ -4,8 +4,10 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
+yarn install --force
 yarn reassure --baseline
 
 # Gather current perf measurements & compare results
 git switch -
+yarn install --force
 yarn reassure
