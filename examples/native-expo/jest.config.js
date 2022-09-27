a97ad860ffafdd12831135d904cf3f32aa511aec
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-09-27 13:08:43 +0200

    fix: remove unnecessary jest-native deps

--- examples/native-expo/jest.config.js
+++ examples/native-expo/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   preset: '@testing-library/react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jest-setup.js'],
+  setupFilesAfterEnv: ['./jest-setup.js'],
 };
