e8634078151c63cbf402af24c4c945e584a6ee81
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-10-04 10:49:17 +0200

    chore: tweak perfs script

--- examples/native/reassure-tests.sh
+++ examples/native/reassure-tests.sh
@@ -4,23 +4,27 @@
 set -e
 
 # Branches are not fetched by default on CI
-git fetch
+git fetch origin
 
 BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Gather baseline perf measurements
+echo Git: switching to baseline branch "$BASELINE_BRANCH"
 git switch "$BASELINE_BRANCH"
 
 # Next line is required because Reassure packages are imported from this monorepo and might require rebuilding.
+echo Rebuilding Reassure packages
 pushd ../.. && yarn install --force && yarn turbo run build && popd
 
 yarn install --force
 yarn reassure --baseline --branch $(git branch --show-current) --commitHash $(git rev-parse HEAD)
 
 # Gather current perf measurements & compare results
+echo Git: switching back to current branch
 git switch -
 
 # Next line is required because Reassure packages are imported from this monorepo and might require rebuilding.
+echo Rebuilding Reassure packages
 pushd ../.. && yarn install --force && yarn turbo run build && popd
 
 yarn install --force
