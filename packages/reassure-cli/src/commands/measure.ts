a5e0afa432bea6765b70c3d5de901dd2086a2fe9
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2023-01-26 09:45:34 +0000

    fix: TEST_RUNNER_PATH usage

--- packages/reassure-cli/src/commands/measure.ts
+++ packages/reassure-cli/src/commands/measure.ts
@@ -36,12 +36,11 @@ export async function run(options: MeasureOptions) {
   const header = { metadata };
   writeFileSync(outputFile, JSON.stringify(header) + '\n');
 
-  const testRunnerPath =
-    process.env.TEST_RUNNER_PATH ?? process.platform === 'win32'
-      ? 'node_modules/jest/bin/jest'
-      : 'node_modules/.bin/jest';
+  const defaultTestRunnerPath = process.platform === 'win32' ? 'node_modules/jest/bin/jest' : 'node_modules/.bin/jest';
+  const testRunnerPath = process.env.TEST_RUNNER_PATH ?? defaultTestRunnerPath;
 
-  const testRunnerArgs = process.env.TEST_RUNNER_ARGS ?? '--runInBand --testMatch "<rootDir>/**/*.perf-test.[jt]s?(x)"';
+  const defaultTestRunnerArgs = '--runInBand --testMatch "<rootDir>/**/*.perf-test.[jt]s?(x)"';
+  const testRunnerArgs = process.env.TEST_RUNNER_ARGS ?? defaultTestRunnerArgs;
 
   const spawnInfo = spawnSync(
     'node',
