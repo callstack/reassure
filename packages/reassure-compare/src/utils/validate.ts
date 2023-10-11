9c97a8a2b57d197a3d55a316fd0005ffa634b088
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-10-11 12:27:59 +0200

    refactor: code review changes

--- packages/reassure-compare/src/utils/validate.ts
+++ packages/reassure-compare/src/utils/validate.ts
@@ -16,31 +16,31 @@ export const performanceHeaderSchema = z.object({
 
 /** Entry in the performance results file. */
 export const performanceEntrySchema = z.object({
-  /** Name of the test scenario */
+  /** Name of the test scenario. */
   name: z.string(),
 
-  /** Type of the test scenario */
+  /** Type of the measured characteristic (render, function execution). */
   type: z.enum(['render', 'function']).default('render'),
 
-  /** Number of times the measurment test was run */
+  /** Number of times the measurement test was run. */
   runs: z.number(),
 
-  /** Arithmetic average of measured durations for each run */
+  /** Arithmetic average of measured render/execution durations for each run. */
   meanDuration: z.number(),
 
-  /** Standard deviation of measured durations for each run */
+  /** Standard deviation of measured render/execution durations for each run. */
   stdevDuration: z.number(),
 
-  /** Array of measured durations for each run */
+  /** Array of measured render/execution durations for each run. */
   durations: z.array(z.number()),
 
-  /** Arithmetic average of measured counts for each run */
+  /** Arithmetic average of measured render/execution counts for each run. */
   meanCount: z.number(),
 
-  /** Standard deviation of measured counts for each run */
+  /** Standard deviation of measured render/execution counts for each run. */
   stdevCount: z.number(),
 
-  /** Array of measured counts for each run */
+  /** Array of measured render/execution counts for each run. */
   counts: z.array(z.number()),
 });
 
--- packages/reassure-measure/src/types.ts
+++ packages/reassure-measure/src/types.ts
@@ -1,28 +1,28 @@
-/** Type (natute) of measured characteristic. */
+/** Type of measured performance characteristic. */
 export type MeasureType = 'render' | 'function';
 
 /**
- * Type representing output of measure functions, e.g. `measurePerformance`, `measureFunction`.
+ * Type representing the result of `measure*` functions.
  */
 export interface MeasureResults {
   /** Number of times the test subject was run */
   runs: number;
 
-  /** Arithmetic average of measured execution durations for each run */
+  /** Arithmetic average of measured render/execution durations for each run */
   meanDuration: number;
 
-  /** Standard deviation of measured execution durations for each run */
+  /** Standard deviation of measured render/execution durations for each run */
   stdevDuration: number;
 
-  /** Array of measured execution durations for each run */
+  /** Array of measured render/execution durations for each run */
   durations: number[];
 
-  /** Arithmetic average of measured execution count for each run */
+  /** Arithmetic average of measured render/execution count for each run */
   meanCount: number;
 
-  /** Standard deviation of measured execution count for each run */
+  /** Standard deviation of measured render/execution count for each run */
   stdevCount: number;
 
-  /** Array of measured execution count for each run */
+  /** Array of measured render/execution count for each run */
   counts: number[];
 }
