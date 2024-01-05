52c71c2b7e0d7f7022e7eecf7a1e398368134aae
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-01-05 11:30:11 +0100

    refactor: rename `meassurePerformance` to `measureRenders` (#433)

--- test-apps/native/.eslintrc.js
+++ test-apps/native/.eslintrc.js
@@ -8,11 +8,7 @@ module.exports = {
     'jest/expect-expect': [
       'error',
       {
-        assertFunctionNames: [
-          'expect',
-          'measurePerformance',
-          'measureFunction',
-        ],
+        assertFunctionNames: ['expect', 'measureRenders', 'measureFunction'],
       },
     ],
   },
