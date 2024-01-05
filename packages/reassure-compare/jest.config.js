dc1d3cbe1ac316acbcc8ce85b12328d69bdee058
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-01-05 16:07:44 +0100

    chore: ignore `lib` in test

--- packages/reassure-compare/jest.config.js
+++ packages/reassure-compare/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@testing-library/react-native',
   transformIgnorePatterns: ['node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)'],
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
   clearMocks: true,
 };
--- packages/reassure-measure/jest.config.js
+++ packages/reassure-measure/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: '@testing-library/react-native',
   setupFilesAfterEnv: ['./jest-setup.ts'],
   transformIgnorePatterns: ['node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)'],
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
   clearMocks: true,
 };
