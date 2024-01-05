50986e0c3e5b0acb6547f6ad8df7d7578c5dc2fd
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-01-05 16:14:44 +0100

    chore: fix tests

--- packages/reassure-measure/jest.config.js
+++ packages/reassure-measure/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   setupFilesAfterEnv: ['./jest-setup.ts'],
   transformIgnorePatterns: ['node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)'],
   modulePathIgnorePatterns: ['<rootDir>/lib/'],
+  snapshotSerializers: ['@relmify/jest-serializer-strip-ansi/always'],
   clearMocks: true,
 };
