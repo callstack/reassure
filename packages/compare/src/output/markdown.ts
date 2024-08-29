759a5e396787d2291e2f37849f08bffc3ac97636
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-08-29 08:53:18 +0200

    refactor: use ts-markdown-builder
    refactor: prototype disclosure
    
    refactor: replace rest of markdown methods
    
    refactor: workarounds for markdown lib
    
    chore: tweaks
    
    chore: tweaks
    
    chore: tweaks
    
    chore: update markdown builder
    
    chore: tweaks
    
    chore: recreate yarn.lock
    
    refactor: adapt markdown generation

--- packages/compare/src/output/markdown.ts
+++ packages/compare/src/output/markdown.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
-// @ts-ignore
-import markdownTable from 'markdown-table';
+import * as md from 'ts-markdown-builder';
 import * as logger from '@callstack/reassure-logger';
 import {
   formatCount,
@@ -11,19 +10,10 @@ import {
   formatCountChange,
   formatDurationChange,
 } from '../utils/format';
-import * as md from '../utils/markdown';
-import type {
-  AddedEntry,
-  CompareEntry,
-  CompareResult,
-  RemovedEntry,
-  MeasureEntry,
-  MeasureMetadata,
-  RenderIssues,
-} from '../types';
-import { collapsibleSection } from '../utils/markdown';
+import { joinBlocks, joinLines } from '../utils/markdown';
+import type { AddedEntry, CompareEntry, CompareResult, RemovedEntry, MeasureEntry, RenderIssues } from '../types';
 
-const tableHeader = ['Name', 'Type', 'Duration', 'Count'] as const;
+const tableHeader = ['Name', 'Type', 'Duration', 'Count'];
 
 export const writeToMarkdown = async (filePath: string, data: CompareResult) => {
   try {
@@ -50,65 +40,64 @@ async function writeToFile(filePath: string, content: string) {
 }
 
 function buildMarkdown(data: CompareResult) {
-  let result = md.heading1('Performance Comparison Report');
-
-  result += `\n${buildMetadataMarkdown('Current', data.metadata.current)}`;
-  result += `\n${buildMetadataMarkdown('Baseline', data.metadata.baseline)}`;
+  let doc = [
+    md.heading('Performance Comparison Report', { level: 1 }),
+    md.list([
+      `${md.bold('Current')}: ${formatMetadata(data.metadata.current)}`,
+      `${md.bold('Baseline')}: ${formatMetadata(data.metadata.baseline)}`,
+    ]),
+  ];
 
   if (data.errors?.length) {
-    result += `\n\n${md.heading3('Errors')}\n`;
-    data.errors.forEach((message) => {
-      result += ` 1. 🛑 ${message}\n`;
-    });
+    doc = [...doc, md.heading('Errors', { level: 2 }), md.list(data.errors)];
   }
 
   if (data.warnings?.length) {
-    result += `\n\n${md.heading3('Warnings')}\n`;
-    data.warnings.forEach((message) => {
-      result += ` 1. 🟡 ${message}\n`;
-    });
+    doc = [...doc, md.heading('Warnings', { level: 2 }), md.list(data.warnings)];
   }
 
-  result += `\n\n${md.heading3('Significant Changes To Duration')}`;
-  result += `\n${buildSummaryTable(data.significant)}`;
-  result += `\n${buildDetailsTable(data.significant)}`;
-  result += `\n\n${md.heading3('Meaningless Changes To Duration')}`;
-  result += `\n${buildSummaryTable(data.meaningless, true)}`;
-  result += `\n${buildDetailsTable(data.meaningless)}`;
+  doc = [
+    ...doc,
+    md.heading('Significant Changes To Duration', { level: 3 }),
+    buildSummaryTable(data.significant),
+    buildDetailsTable(data.significant),
+    md.heading('Meaningless Changes To Duration', { level: 3 }),
+    buildSummaryTable(data.meaningless, true),
+    buildDetailsTable(data.meaningless),
+  ];
 
   // Skip renders counts if user only has function measurements
   const allEntries = [...data.significant, ...data.meaningless, ...data.added, ...data.removed];
   const hasRenderEntries = allEntries.some((e) => e.type === 'render');
   if (hasRenderEntries) {
-    result += `\n\n${md.heading3('Render Count Changes')}`;
-    result += `\n${buildSummaryTable(data.countChanged)}`;
-    result += `\n${buildDetailsTable(data.countChanged)}`;
-    result += `\n\n${md.heading3('Render Issues')}`;
-    result += `\n${buildRenderIssuesTable(data.renderIssues)}`;
+    doc = [
+      ...doc,
+      md.heading('Render Count Changes', { level: 3 }),
+      buildSummaryTable(data.countChanged),
+      buildDetailsTable(data.countChanged),
+      md.heading('Render Issues', { level: 3 }),
+      buildRenderIssuesTable(data.renderIssues),
+    ];
   }
 
-  result += `\n\n${md.heading3('Added Scenarios')}`;
-  result += `\n${buildSummaryTable(data.added)}`;
-  result += `\n${buildDetailsTable(data.added)}`;
-  result += `\n\n${md.heading3('Removed Scenarios')}`;
-  result += `\n${buildSummaryTable(data.removed)}`;
-  result += `\n${buildDetailsTable(data.removed)}`;
-  result += '\n';
+  doc = [
+    ...doc,
+    md.heading('Added Entries', { level: 3 }),
+    buildSummaryTable(data.added),
+    buildDetailsTable(data.added),
+    md.heading('Removed Entries', { level: 3 }),
+    buildSummaryTable(data.removed),
+    buildDetailsTable(data.removed),
+  ];
 
-  return result;
-}
-
-function buildMetadataMarkdown(name: string, metadata: MeasureMetadata | undefined) {
-  return ` - ${md.bold(name)}: ${formatMetadata(metadata)}`;
+  return joinBlocks(doc.filter(Boolean));
 }
 
 function buildSummaryTable(entries: Array<CompareEntry | AddedEntry | RemovedEntry>, collapse: boolean = false) {
   if (!entries.length) return md.italic('There are no entries');
 
   const rows = entries.map((entry) => [entry.name, entry.type, formatEntryDuration(entry), formatEntryCount(entry)]);
-  const content = markdownTable([tableHeader, ...rows]);
-
-  return collapse ? collapsibleSection('Show entries', content) : content;
+  return md.disclosure('Show entries', md.table(tableHeader, rows), { open: !collapse });
 }
 
 function buildDetailsTable(entries: Array<CompareEntry | AddedEntry | RemovedEntry>) {
@@ -120,9 +109,8 @@ function buildDetailsTable(entries: Array<CompareEntry | AddedEntry | RemovedEnt
     buildDurationDetailsEntry(entry),
     buildCountDetailsEntry(entry),
   ]);
-  const content = markdownTable([tableHeader, ...rows]);
 
-  return collapsibleSection('Show details', content);
+  return md.disclosure('Show details', md.table(tableHeader, rows));
 }
 
 function formatEntryDuration(entry: CompareEntry | AddedEntry | RemovedEntry) {
@@ -141,49 +129,41 @@ function formatEntryCount(entry: CompareEntry | AddedEntry | RemovedEntry) {
 }
 
 function buildDurationDetailsEntry(entry: CompareEntry | AddedEntry | RemovedEntry) {
-  return [
+  return joinBlocks([
     entry.baseline != null ? buildDurationDetails('Baseline', entry.baseline) : '',
     'current' in entry ? buildDurationDetails('Current', entry.current) : '',
-  ]
-    .filter(Boolean)
-    .join('<br/><br/>');
+  ]);
 }
 
 function buildCountDetailsEntry(entry: CompareEntry | AddedEntry | RemovedEntry) {
-  return [
+  return joinBlocks([
     entry.baseline != null ? buildCountDetails('Baseline', entry.baseline) : '',
     'current' in entry ? buildCountDetails('Current', entry.current) : '',
-  ]
-    .filter(Boolean)
-    .join('<br/><br/>');
+  ]);
 }
 
 function buildDurationDetails(title: string, entry: MeasureEntry) {
   const relativeStdev = entry.stdevDuration / entry.meanDuration;
 
-  return [
+  return joinLines([
     md.bold(title),
     `Mean: ${formatDuration(entry.meanDuration)}`,
     `Stdev: ${formatDuration(entry.stdevDuration)} (${formatPercent(relativeStdev)})`,
     entry.durations ? `Runs: ${formatRunDurations(entry.durations)}` : '',
     entry.warmupDurations ? `Warmup runs: ${formatRunDurations(entry.warmupDurations)}` : '',
-  ]
-    .filter(Boolean)
-    .join(`<br/>`);
+  ]);
 }
 
 function buildCountDetails(title: string, entry: MeasureEntry) {
   const relativeStdev = entry.stdevCount / entry.meanCount;
 
-  return [
+  return joinLines([
     md.bold(title),
     `Mean: ${formatCount(entry.meanCount)}`,
     `Stdev: ${formatCount(entry.stdevCount)} (${formatPercent(relativeStdev)})`,
     entry.counts ? `Runs: ${entry.counts.map(formatCount).join(' ')}` : '',
     buildRenderIssuesList(entry.issues),
-  ]
-    .filter(Boolean)
-    .join(`<br/>`);
+  ]);
 }
 
 function formatRunDurations(values: number[]) {
@@ -193,14 +173,14 @@ function formatRunDurations(values: number[]) {
 function buildRenderIssuesTable(entries: Array<CompareEntry | AddedEntry>) {
   if (!entries.length) return md.italic('There are no entries');
 
-  const tableHeader = ['Name', 'Initial Updates', 'Redundant Updates'] as const;
+  const tableHeader = ['Name', 'Initial Updates', 'Redundant Updates'];
   const rows = entries.map((entry) => [
     entry.name,
     formatInitialUpdates(entry.current.issues?.initialUpdateCount),
     formatRedundantUpdates(entry.current.issues?.redundantUpdates),
   ]);
 
-  return markdownTable([tableHeader, ...rows]);
+  return md.table(tableHeader, rows);
 }
 
 function buildRenderIssuesList(issues: RenderIssues | undefined) {
@@ -208,13 +188,13 @@ function buildRenderIssuesList(issues: RenderIssues | undefined) {
 
   const output = ['Render issues:'];
   if (issues?.initialUpdateCount) {
-    output.push(` - Initial updates: ${formatInitialUpdates(issues.initialUpdateCount, false)}`);
+    output.push(`- Initial updates: ${formatInitialUpdates(issues.initialUpdateCount, false)}`);
   }
   if (issues?.redundantUpdates?.length) {
-    output.push(` - Redundant updates: ${formatRedundantUpdates(issues.redundantUpdates, false)}`);
+    output.push(`- Redundant updates: ${formatRedundantUpdates(issues.redundantUpdates, false)}`);
   }
 
-  return output.join('<br/>');
+  return output.join('\n');
 }
 
 function formatInitialUpdates(count: number | undefined, showSymbol: boolean = true) {
