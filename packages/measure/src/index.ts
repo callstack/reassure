6858cf4a61bc45e6a05d22cb0aadf035ba1ebf48
Fábio Henriques <contact@fabiohenriques.dev>
2025-03-04 16:55:52 +0000

    feat: add `measureAsyncFunction` function

--- packages/measure/src/index.ts
+++ packages/measure/src/index.ts
@@ -1,6 +1,8 @@
 export { configure, resetToDefaults } from './config';
 export { measureRenders, measurePerformance } from './measure-renders';
 export { measureFunction } from './measure-function';
+export { measureAsyncFunction } from './measure-async-function';
 export type { MeasureRendersOptions } from './measure-renders';
 export type { MeasureFunctionOptions } from './measure-function';
+export type { MeasureAsyncFunctionOptions } from './measure-async-function';
 export type { MeasureType, MeasureResults } from './types';
