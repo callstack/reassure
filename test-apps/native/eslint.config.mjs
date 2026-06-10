e263cc429416d78dec3bbdc855ab63b3aff6d836
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-10 15:32:48 +0200

    .

--- test-apps/native/eslint.config.mjs
+++ test-apps/native/eslint.config.mjs
@@ -1,32 +1,9 @@
-import { fixupPluginRules } from '@eslint/compat';
+import { fixupConfigRules } from '@eslint/compat';
 import reactNativeConfig from '@react-native/eslint-config/flat';
-
-function patchLegacyPlugins(configItem) {
-  const { plugins } = configItem;
-  if (!plugins) {
-    return configItem;
-  }
-
-  let patchedPlugins = plugins;
-
-  for (const pluginName of ['eslint-comments', 'react', 'react-native']) {
-    if (plugins[pluginName]) {
-      patchedPlugins =
-        patchedPlugins === plugins ? { ...plugins } : patchedPlugins;
-      patchedPlugins[pluginName] = fixupPluginRules(plugins[pluginName]);
-    }
-  }
-
-  return patchedPlugins === plugins
-    ? configItem
-    : { ...configItem, plugins: patchedPlugins };
-}
-
-const patchedReactNativeConfig = reactNativeConfig.map(patchLegacyPlugins);
 
 export default [
   {
     ignores: ['eslint.config.mjs'],
   },
-  ...patchedReactNativeConfig,
+  ...fixupConfigRules(reactNativeConfig),
 ];
