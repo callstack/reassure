d1d15a04d85ffc144fbcb5c98553a0c8c8619f0d
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-10 16:57:22 +0200

    .

--- test-apps/native/eslint.config.mjs
+++ test-apps/native/eslint.config.mjs
@@ -1,9 +1,4 @@
 import { fixupConfigRules } from '@eslint/compat';
 import reactNativeConfig from '@react-native/eslint-config/flat';
 
-export default [
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  ...fixupConfigRules(reactNativeConfig),
-];
+export default [...fixupConfigRules(reactNativeConfig)];
