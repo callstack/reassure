80f048b793d9ee7ae180cf7d0eaea61d192e88c6
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-15 09:04:02 +0200

    chore: replace prettier by oxfmt

--- test-apps/native/eslint.config.mjs
+++ test-apps/native/eslint.config.mjs
@@ -1,4 +1,9 @@
 import { fixupConfigRules } from '@eslint/compat';
 import reactNativeConfig from '@react-native/eslint-config/flat';
+import pluginOxfmt from 'eslint-plugin-oxfmt';
 
-export default [...fixupConfigRules(reactNativeConfig)];
+export default [
+  ...fixupConfigRules(reactNativeConfig),
+  { rules: { 'prettier/prettier': 'off' } },
+  pluginOxfmt.configs.recommendedWithoutParser,
+];
